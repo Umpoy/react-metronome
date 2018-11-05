10038f915a2ff7a69b8e050333aef5ed18896b32
Umpoy <iumpoy@yahoo.com>
2018-11-04 19:34:23 -0800

    Metronome UI added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>
+    let bpm = 100;
+    let playing = false;
+    return (
+      <div className="app">
+        <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <input type="range" min="60" max="240" value={bpm} />
+        </div>
+        <button>{playing ? 'Stop' : 'Start'}</button>
+      </div>
+    )
   }
 }
 
