91451db90802c3abf592f3bc00a04a09fdc9291d
Umpoy <iumpoy@yahoo.com>
2018-11-06 21:33:04 -0800

    beats can now change when you move slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,7 +20,17 @@ class App extends Component {
 
   handleBPMChange = event => {
     const bpm = event.target.value;
-    this.setState({ bpm });
+    if (this.state.playing) {
+      clearInterval(this.timer);
+      this.timer = setInterval(this.playClick, (60 / bpm) * 1000);
+
+      this.setState({
+        count: 0,
+        bpm
+      });
+    } else {
+      this.setState({ bpm });
+    }
   }
 
   clickSound = () => {
